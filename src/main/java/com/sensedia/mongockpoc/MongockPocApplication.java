ca78957f159a81de3db49649ee6253fae6ecb6c1
Dejamir <dejamir.gianelli@gmail.com>
2022-04-25 14:03:00 -0300

    feat: First commit

---
+++ src/main/java/com/sensedia/mongockpoc/MongockPocApplication.java
@@ -0,0 +1,13 @@
+package com.sensedia.mongockpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongockPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongockPocApplication.class, args);
+	}
+
+}
